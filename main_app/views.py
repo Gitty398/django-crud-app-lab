955fab114f663e43d387389590cbb1656f362416
John F <anabasisinc@outlook.com>
2026-02-20 21:53:54 -0500

    completed initial setup with home and about route with some css

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Welcome to the Historical Marker Collector!</h1>')
+
+def about(request):
+    return render(request, 'about.html')
