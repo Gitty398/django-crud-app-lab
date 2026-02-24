9b4db180c6cc76d57bfdde560fb6d6189e3cf869
John F <anabasisinc@outlook.com>
2026-02-23 20:11:30 -0500

    completed exercise up to CreateView component

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('markers/', views.marker_index, name='marker-index'),
     path('markers/<int:marker_id>/', views.marker_detail, name='marker-detail'),
-    # path('create/', views.create, name='create'),
+    path('markers/create/', views.MarkerCreate.as_view(), name='marker-create')
     # path('update/', views.update, name='update'),
     # path('delete/', views.delete, name='delete'),
 ]
